ca5ef193eba4a18fda7fe24eaece249d4ebcc14e
Mahmoud <mahmoudhany374@gmail.com>
2024-10-07 01:34:20 +0300

    The project is created

---
+++ app/src/main/java/com/example/yumm/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.yumm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+    }
+}
